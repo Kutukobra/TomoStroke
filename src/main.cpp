03f2cca35e7882b11e53aacae5b3d9f2ea253431
Kutukobra <nadzhiff@gmail.com>
2025-11-30 00:49:34 +0700

    Project init

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+}
